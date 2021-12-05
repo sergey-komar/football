3e7625359f977afb9b8089a41e7f297cd834f040
sergey-komar <kursev81@yandex.ru>
2021-12-06 00:00:54 +0300

    start project

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,31 @@
 $(function () {
+    $('.slider').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      
+        // appendArrows:'.arr',
+        prevArrow: ' <img src="./images/home/arrow (1).png" class="slick-prev-header" alt="">',
+	    nextArrow: '<img src="./images/home/arrow.png" class="slick-next-header" alt="">',
+       
+    })
 
+
+    $('.camp-slider__inner').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        fade: true,
+        appendArrows: '.camp-slider__arrows',
+        prevArrow: ' <img src="./images/home/icon/camp-arrow.svg" alt="arrow">',
+	    nextArrow: '<img src="./images/home/icon/camp-arroww.svg" alt="arrow">',
+        infinite: true,
+        dots: true,
+    });
+
+    $('.camp-slider__inner').on('afterChange', function(event, slick, currentSlide){
+        $("#test").text(currentSlide + 1);
+     });
+   
+
+
+    
 })
