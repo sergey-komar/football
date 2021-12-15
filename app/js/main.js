0b2979d13e9ca4bb070300d8b13d89107c135045
sergey-komar <kursev81@yandex.ru>
2021-12-15 23:14:54 +0300

    two page

--- app/js/main.js
+++ app/js/main.js
@@ -25,7 +25,57 @@ $(function () {
         $("#test").text(currentSlide + 1);
      });
    
-
-
-    
+// ACCARDION
+$('.accardion__btn').on('click', function(){
+   $(this).next().slideToggle(500); 
 })
+
+// TRAINERS
+$('.trainers-slider__inner').slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    appendArrows: '.trainers__arrows',
+    prevArrow: ' <img src="./images/home/icon/camp-arrow.svg" alt="arrow">',
+    nextArrow: '<img src="./images/home/icon/camp-arroww.svg" alt="arrow">',
+});
+
+
+// SCHOOLL
+$('.school-slider__inner').slick({
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    appendArrows: '.school__arrows',
+    prevArrow: ' <img src="./images/home/icon/camp-arrow.svg" alt="arrow">',
+    nextArrow: '<img src="./images/home/icon/camp-arroww.svg" alt="arrow">',
+});
+
+// REVIEWS
+$('.reviews-slider__inner').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    appendArrows: '.reviews__arrows',
+    prevArrow: ' <img src="./images/home/icon/camp-arrow.svg" alt="arrow">',
+    nextArrow: '<img src="./images/home/icon/camp-arroww.svg" alt="arrow">',
+});
+$('.reviews-slider__inner').on('afterChange', function(event, slick, currentSlide){
+    $("#curent").text(currentSlide + 1);
+ });
+
+
+
+//  SEASON
+$('.season-slider__inner').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    appendArrows: '.season-slider__arrows',
+    prevArrow: '  <img src="./images/winter/season-arrow-left.svg" alt="">',
+    nextArrow: ' <img src="./images/winter/season-arrow-right.svg" alt="">',
+    infinite: true,
+    dots: true,
+})
+$('.season-slider__inner').on('afterChange', function(event, slick, currentSlide){
+    $("#test").text(currentSlide + 1);
+ });
+
+
+});
